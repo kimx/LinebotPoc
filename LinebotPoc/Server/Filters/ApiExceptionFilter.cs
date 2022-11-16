b978634734e7a2f4dea72c01423581ab02d76a0d
kim <kim@bud4.net>
2022-11-16 15:05:31 +0800

    NLog

---
+++ LinebotPoc/Server/Filters/ApiExceptionFilter.cs
@@ -0,0 +1,26 @@
+﻿using LinebotPoc.Server.Pages;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace LinebotPoc.Server.Filters;
+public class ApiExceptionFilter : IExceptionFilter
+{
+    private ILogger<ApiExceptionFilter> _log;
+    public ApiExceptionFilter(ILogger<ApiExceptionFilter> log)
+    {
+        _log = log;
+
+    }
+
+    public void OnException(ExceptionContext context)
+    {
+        _log.LogError(context.Exception, context.Exception.Message);
+
+
+    }
+
+
+
+
+}
---
+++ LinebotPoc/Server/Filters/LogFilter.cs
@@ -0,0 +1,27 @@
+﻿using LinebotPoc.Server.Pages;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace LinebotPoc.Server.Filters;
+public class LogFilter : IActionFilter
+{
+    private ILogger<LogFilter> _log;
+    public LogFilter(ILogger<LogFilter> log)
+    {
+        _log = log;
+    }
+    public void OnActionExecuting(ActionExecutingContext context)
+    {
+        _log.LogInformation($"{context.ActionDescriptor.DisplayName}");
+    }
+
+    public void OnActionExecuted(ActionExecutedContext context)
+    {
+    }
+
+
+
+
+
+}
